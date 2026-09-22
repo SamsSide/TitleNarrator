7f93ac6f51b88bb17e91466731ace9cbf221a8fa
SamsSide <samuelamakin@gmail.com>
2026-09-22 22:23:32 +0100

    build: scaffold Fabric 26.2 client-only project

---
+++ src/client/java/dev/samsside/titlenarrator/TitleNarratorClient.java
@@ -0,0 +1,9 @@
+package dev.samsside.titlenarrator;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class TitleNarratorClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
